41f7bacbe7565f0a3a493a9cd48ab246dc6eebd3
Joji <jojijoseph@iisc.ac.in>
2024-09-19 17:11:05 +0530

    Add eval script for affordance transfer

---
+++ eval_affordance_transfer.sh
@@ -0,0 +1,4 @@
+python affordance_transfer_pipeline.py --data-dir "./data/processed_scene_01" --checkpoint "./data/processed_scene_01/ckpts/ckpt_29999_rank0.pt" --labels_dir "./data/affordance_labels" --results-dir "./results/scene_01"
+python affordance_transfer_pipeline.py --data-dir "./data/processed_scene_02" --checkpoint "./data/processed_scene_02/ckpts/ckpt_29999_rank0.pt" --labels_dir "./data/affordance_labels" --results-dir "./results/scene_02"
+python affordance_transfer_pipeline.py --data-dir "./data/processed_scene_03" --checkpoint "./data/processed_scene_03/ckpts/ckpt_29999_rank0.pt" --labels_dir "./data/affordance_labels" --results-dir "./results/scene_03"
+
